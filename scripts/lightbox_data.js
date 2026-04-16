817994f3eeb93780d222f9f93b8ce4555fc4e98f
gabe-the-coder <gabarba21@amarillocollege.com>
2026-04-16 18:59:06 -0500

    new scripts
    new scripts for the photo gallery

---
+++ scripts/lightbox_data.js
@@ -0,0 +1,27 @@
+"use strict";
+/*    
+      Image List
+
+      Filename:lightbox_data.js
+*/
+
+// Title of the slideshow
+let lightboxTitle = "Gabe's motel and sister location";
+
+// Names of the image files shown in the slideshow
+let imgFiles = ["images/newlocation.png", "images/gym.png", "images/dining.png", "images/kitchen.png",
+                "images/lobby.png", "images/loung.png", "images/giftshop.png",]
+
+// Captions associated with each image
+let imgCaptions = new Array(12);
+imgCaptions[0]="New location 'Sunset Rest' (On the Canyon E-way)";
+imgCaptions[1]="Updated gym facilites 'Gabe's Motel' (Available in both locations, open 24/7)"; 
+imgCaptions[2]="Dining room 'Gabe's Motel' (Available in both locations, open 24/7)"; 
+imgCaptions[3]="Kitchen 'Sunset Rest' (Available in both locations, serve all three meals with a late night menu, open 24/7)"; 
+imgCaptions[4]="Lobby 'Gabe's Motel' (Clean lobbies in both locations)";
+imgCaptions[5]="Outdoor loung 'Sunset Rest' (Clean outdoor loung to enjoy the outdoors)";
+imgCaptions[6]="Giftshop 'Sunset Rest' (A local gift shop that we collaborate with that sells local merch)";
+
+
+// Count of images in the slideshow
+let imgCount = imgFiles.length;
